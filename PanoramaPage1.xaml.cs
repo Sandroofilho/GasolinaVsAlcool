e93e1ad6301ba2b0077032c9998c73a7711fc76c
Sandro Filho <sandroofilho@gmail.com>
2014-12-09 09:42:27 -0200

    APP GASOLINA OU ALCOOL
    Modelo de Cadastro e Panorama

---
+++ PanoramaPage1.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Gasolina_ou_Alcool.Classes;
+using Microsoft.Phone.Controls;
+using Gasolina_ou_Alcool.Resources;
+
+namespace Gasolina_ou_Alcool
+{
+    public partial class PanoramaPage1 : PhoneApplicationPage
+    {
+        clsCombustiveis combustivel;
+        public PanoramaPage1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            AtualizarLista();
+            AtualizarLista2();
+        }
+
+
+
+
+
+        //Método amarrado ao evento para cadastrar um novo Posto
+        private void onClickNovo(object sender, EventArgs e)
+        {
+            CadastraPosto();
+        }
+
+
+        private void CadastraPosto()
+        {
+            NavigationService.Navigate(new Uri("/cadastraPosto.xaml", UriKind.Relative));
+        }
+
+
+        private void AtualizarLista2()
+        {
+            List<clsCombustiveis> lista = clsCombustiveisDB.GetComb("F1");
+            lstCombustiveis2.ItemsSource = lista;
+        }
+
+
+        private void AtualizarLista()
+        {
+            List<clsCombustiveis> lista = clsCombustiveisDB.GetCombustiveis("F1");
+            lstCombustiveis.ItemsSource = lista;
+        }
+
+        private void OnClickDeletar(object sender, EventArgs e)
+        {
+
+
+            if (MessageBox.Show("Deletar " + combustivel.Posto + "?", "Atenção",
+                                MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            {
+                clsCombustiveisDB.Deletar(combustivel);
+                AtualizarLista();
+                AtualizarLista2();
+            }
+
+
+        }
+
+        private void OnClickAtualizar(object sender, EventArgs e)
+        {
+            AtualizarLista();
+            AtualizarLista2();
+
+        }
+
+        private void OnSelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+            //vai instanciar o objeto selecionado na listBox
+            combustivel = (sender as ListBox).SelectedItem as clsCombustiveis;
+        }
+
+        private void OnClickEditar(object sender, EventArgs e)
+        {
+            CadastraPosto();
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            cadastraPosto page = e.Content as cadastraPosto;
+            if (page != null)
+            {
+                page.Combustivel = combustivel;
+                page.tituloPage.Text = "Qual o ideal?";
+
+            }
+        }
+
+        private void lstCombustiveis_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            List<clsCombustiveis> lista = clsCombustiveisDB.GetCombustiveis("F1");
+            lstCombustiveis.ItemsSource = lista;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            List<clsCombustiveis> lista = clsCombustiveisDB.GetComb("F1");
+            lstCombustiveis.ItemsSource = lista;
+        }
+
+        private void Panorama_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //
+
+
+
+
+
+       
+    }
+}
---
+++ cadastraPosto.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Gasolina_ou_Alcool.Classes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Gasolina_ou_Alcool.Resources;
+
+namespace Gasolina_ou_Alcool
+{
+    public partial class cadastraPosto : PhoneApplicationPage
+    {
+        public clsCombustiveis Combustivel { get; set; }
+
+        public cadastraPosto()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (Combustivel != null)
+            {
+                txtResultado.Text = Combustivel.Resultado;
+                txtPosto.Text = Combustivel.Posto;
+            }
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Novo()
+        {
+
+
+        }
+
+        private void Alterar()
+        {
+           
+
+
+            Combustivel.Resultado = txtResultado.Text;
+            Combustivel.Posto = txtPosto.Text;
+            Combustivel.Alcool = txtalcool.Text;
+            Combustivel.Gasolina = txtgasolina.Text;
+            
+
+        
+            clsCombustiveisDB.Atualizar(Combustivel);
+
+        }
+
+        private void OnClickSalvar(object sender, EventArgs e)
+        {
+            //Atributo DataTime
+            DateTime myValue = DateTime.Now;
+            DateTime myBirthday = DateTime.Parse("12/07/1987");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            txtRegistro.Text = myValue.AddDays(0).ToShortDateString();
+
+            //           if (gasolina.Text == string.Empty)
+            //         {
+            //           MessageBox.Show("Faltando Item");
+            //   }
+
+            double gasol = double.Parse(txtgasolina.Text);
+            double alco = double.Parse(txtalcool.Text);
+            double soma = alco / gasol;
+            if (soma <= 0.7)
+            {
+                txtResultado.Text = ("Abastecer com Álcool!");
+            }
+            else
+            {
+                txtResultado.Text = ("Abastecer com Gasolina!");
+            }
+
+            if (Combustivel != null) // alterar
+            {
+                //atualiza o objeto com os dados digitados pelo usuário
+                Combustivel.Resultado = txtResultado.Text;
+                Combustivel.Posto = txtPosto.Text;
+                Combustivel.Alcool = txtalcool.Text;
+                Combustivel.Gasolina = txtgasolina.Text;
+
+                clsCombustiveisDB.Atualizar(Combustivel);
+            }
+            else
+            {
+                Combustivel = new clsCombustiveis
+                {
+                    Resultado = txtResultado.Text,
+                    Posto = txtPosto.Text,
+                    Registro = txtRegistro.Text,
+                    Gasolina = txtgasolina.Text,
+                    Alcool = txtalcool.Text
+
+                };
+                clsCombustiveisDB.Salvar(Combustivel);
+            }
+
+
+            NavigationService.GoBack();
+        }
+
+        private void gasolina_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void alcool_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtPosto_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+   
+
+    }
+}
